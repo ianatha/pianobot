81fcf1d6500bde9c399e32eb39e9fe570ad284bc
Ian Atha <iwa@openai.com>
2019-01-19 12:12:32 -0800

    Provide feedback by playing sounds through the piano.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import logging
 import sys
 import time
-from rtmidi.midiutil import open_midiinput
+from rtmidi.midiutil import open_midiinput, open_midioutput
 from rtmidi.midiconstants import NOTE_OFF, NOTE_ON
 from midiutil import MIDIFile
 import os
@@ -14,6 +14,33 @@ NUMBER_OF_PIANO_KEYS = 120
 log = logging.getLogger('pianobot')
 logging.basicConfig(level=logging.DEBUG)
 
+class MusicalFeedback(object):
+    def __init__(self, out):
+        self._out = out
+
+    def _chord(self, notes, duration_in_secs):
+        for note in notes:
+            self._out.send_message([NOTE_ON, note, 112])
+        time.sleep(duration_in_secs)
+        for note in notes:
+            self._out.send_message([NOTE_OFF, note, 0])
+
+    def _play_notes(self, notes, duration_in_secs, velocity):
+        for note in notes:
+            self._out.send_message([NOTE_ON, note, velocity])
+            time.sleep(duration_in_secs)
+            self._out.send_message([NOTE_OFF, note, velocity])
+
+    def sad_sound(self):
+        self._play_notes([60, 60, 59, 59, 58, 58, 57, 57], 0.1, 120)
+
+    def happy_sound(self):
+        self._play_notes([100, 100, 101, 101, 102, 102, 103, 103], 0.1, 120)
+
+    def happy_chords(self):
+        self._chord([60, 64, 67], 0.5)
+        self._chord([61, 65, 68], 0.5)
+        self._chord([60, 64, 67], 0.5)
 
 class Keyboard(object):
     def __init__(self):
@@ -64,14 +91,18 @@ class MidiInCallback(object):
 def main():
     try:
         midi_in, in_port_name = open_midiinput(PORT_NUMBER)
+        midi_out, out_port_name = open_midioutput(PORT_NUMBER, 0)
     except (EOFError, KeyboardInterrupt):
         sys.exit()
+
+    musical_feedback = MusicalFeedback(midi_out)
 
     keyboard = Keyboard()
 
     midi_in.set_callback(MidiInCallback(keyboard))
 
     print("Entering main Pianobot loop. Press Control-C to exit.")
+    musical_feedback.happy_chords()
     try:
         while True:
             time.sleep(1)
@@ -80,7 +111,9 @@ def main():
     finally:
         print("Exit.")
         midi_in.close_port()
+        midi_out.close_port()
         del midi_in
+        del midi_out
 
 
 if __name__ == "__main__":
